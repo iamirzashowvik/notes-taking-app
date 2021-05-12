2f12b3bfc70b71bb0cb6c17cd80bb92e328bbf39
iamirzashowvik <showvikmirza@gmail.com>
2021-05-12 22:29:07 +0600

    add

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+const https = require("https");
+var app = express();
+var today = Date();
+const port = 3000;
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  
+
+  res.render("list", { date: today ,note:notes});
+});
+app.post("/", function (req, res) {
+  console.log(req.body);
+ notes.push(req.body.num1)
+ res.render("list", { date: today ,note:notes});
+});
+var notes=[
+  'milk'
+]
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
